157a66252fc2a1fc0f94dd3b01037db5ca02dfbe
ubiquitous-dev <ubiquitousobjects@protonmail.com>
2021-12-26 23:30:14 -0800

    Add material ui and local icons/typefaces

--- apps/01_uo_onboarding/src/App.tsx
+++ apps/01_uo_onboarding/src/App.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
+import ThreeDRotation from '@mui/icons-material/ThreeDRotation';
+import { Typography } from '@mui/material';
 
 function App() {
   return (
@@ -16,6 +23,21 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
+
+      <Typography variant="h1" component="div" gutterBottom>
+        h1. Heading
+      </Typography>
+      <Typography variant="h2" gutterBottom component="div">
+        h2. Heading
+      </Typography>
+      <Typography variant="h3" gutterBottom component="div">
+        h3. Heading
+      </Typography>
+      <Typography variant="h4" gutterBottom component="div">
+        h4. Heading
+      </Typography>
+          <AccessAlarmIcon></AccessAlarmIcon>
+          <ThreeDRotation>  </ThreeDRotation>
           Learn React
         </a>
       </header>
