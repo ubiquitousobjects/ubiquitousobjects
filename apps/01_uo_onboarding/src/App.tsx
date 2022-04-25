e9534e2812c456affa57f4ce41810c163055a7da
ubiquitous-dev <ubiquitousobjects@protonmail.com>
2022-04-25 15:20:09 -0700

    Add comments to onboarding app

--- apps/01_uo_onboarding/src/App.tsx
+++ apps/01_uo_onboarding/src/App.tsx
@@ -9,13 +9,47 @@ import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
 import ThreeDRotation from '@mui/icons-material/ThreeDRotation';
 import { Typography } from '@mui/material';
 
+
+/*
+
+          Root User Name (?? or just force 'root', maybe in disabled eleemnt?)
+          Root User Password
+          Confirm Password
+
+          Local Development or Customize
+          (customize isn't implemented yet - not MVP?)
+          these are customization settings
+
+          Storage Configuration (2nd step in wizard) ( note: defaults are perfect for local development.)
+          Ephemeral Storage Backend ( ? in-memory.  needs to be synced if distributed.  redis or just local)
+          Persistent Storage Backend (? database, in-memory SQLite, regular sqlite)
+          File Storage Backend ( attachments, like images)
+
+          Network Configuration
+          CORS
+          Port for websocket
+
+          Once wizard is up you log in as root user and after that it's the actual UO app.
+
+          <!-- this all posts to API and creates config file.  once config file is present, it's in locked down mode. -->
+          admin mode only available to localhost
+
+
+*/
+
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+        <FormControl>
+  <InputLabel htmlFor="my-input">Email address</InputLabel>
+  <Input id="my-input" aria-describedby="my-helper-text" />
+  <FormHelperText id="my-helper-text">We'll never share your email.</FormHelperText>
+</FormControl>
         </p>
         <a
           className="App-link"
