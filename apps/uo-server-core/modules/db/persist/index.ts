9d591228329a4e56e9e59250bd33e6d83ba8a5b9
ubiquitous-dev <ubiquitousobjects@protonmail.com>
2022-12-03 19:22:47 -0800

    Add stub files for different implementation

---
+++ apps/uo-server-core/modules/db/persist/index.ts
@@ -0,0 +1,10 @@
+import { Id } from "../../shared/identifier.ts";
+
+
+export async function createSchema(schema: object, version: Id) {
+
+}
+
+export async function saveFile(schemaVersion: version) {
+    
+}
---
+++ apps/uo-server-core/modules/storage/index.ts
@@ -0,0 +1,8 @@
+// TODO: store files and return an ID for the file.  Support a file backend as well as S3-compatible.
+
+
+export async function storeFile() {}
+
+export async function listFiles(path: string) {}
+
+export async function getFile(path: string){}
