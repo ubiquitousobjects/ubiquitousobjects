2528f6b38aef1ea1b8905bdb03edcecf0049fb8e
ubiquitous-dev <ubiquitousobjects@protonmail.com>
2022-12-29 17:39:18 -0800

    Add function pool implementation with executor

--- apps/ubiquitous-objects/Program.cs
+++ apps/ubiquitous-objects/Program.cs
@@ -1,4 +1,6 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Wasmtime;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -15,6 +17,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseDefaultFiles();
+
+app.UseStaticFiles();
 
 var summaries = new[]
 {
@@ -23,7 +28,7 @@ var summaries = new[]
 
 app.MapGet("/weatherforecast", () =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
+    var forecast = Enumerable.Range(1, 2).Select(index =>
         new WeatherForecast
         (
             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
@@ -35,6 +40,48 @@ app.MapGet("/weatherforecast", () =>
 })
 .WithName("GetWeatherForecast")
 .WithOpenApi();
+
+
+using var engine = new Engine();
+
+using var module = Module.FromText(
+    engine,
+    "hello",
+    "(module (func $hello (import \"\" \"hello\")) (func (export \"run\") (call $hello)))"
+);
+
+using var linker = new Linker(engine);
+
+using var store = new Store(engine);
+
+async string runWasmFunction()
+{
+    var instance = linker.Instantiate(store, module);
+    linker.Define(
+        "",
+        "hello",
+        Function.FromCallback(store, () => Console.WriteLine("Hello from C#!"))
+    );
+
+    await Task.Run(() =>
+    {
+        var run = instance.GetAction("run");
+    }
+
+}
+
+app.MapGet("/wasmtime", () =>
+{
+
+
+    run();
+}
+);
+
+// TODO: Serve static files from DB
+// Support publishing files to UO and then serving versions
+
+
 
 app.Run();
 
---
+++ apps/ubiquitous.functions.tests.unit/TestFunctionPool.cs
@@ -0,0 +1,11 @@
+﻿namespace ubiquitous.functions.tests.unit;
+
+public class TestFunctionPool
+{
+    [Fact]
+    public void ExecuteTypescriptFunctionFromPool()
+    {
+        var pool = new FunctionPool();
+        pool.ExecuteFunction("a", "b");
+    }
+}
